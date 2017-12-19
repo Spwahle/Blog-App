9bda6cf59c3ed73d2f5d5c8156595fc5d01d378b
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-18 19:37:21 -0800

    adds new schema

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ mongoose.connect('mongodb://localhost/blog_app');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+
+//M ongoose/Model Confog
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+let Blog = mongoose.model('Blog', blogSchema);
+
+//RESTFUL ROUTES
+
+app.listen(3000, function() {
+    console.log('The BlogApp Server has started');
+});
