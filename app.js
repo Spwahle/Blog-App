458fae484cef9a31002fc9f891e21c76b4730761
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-18 19:40:15 -0800

    testing mongodb

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ let mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/blog_app');
 
+//APP set up
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-//M ongoose/Model Confog
+//Mongoose/Model Confog
 let blogSchema = new mongoose.Schema({
     title: String,
     image: String,
@@ -21,6 +22,14 @@ let blogSchema = new mongoose.Schema({
 let Blog = mongoose.model('Blog', blogSchema);
 
 //RESTFUL ROUTES
+
+Blog.create({
+    title: 'Test Blog',
+    image: 'https://unsplash.com/photos/6yiq2HPIW7Q',
+    body: 'HELLO THIS A BLOG THINGER',
+});
+
+
 
 app.listen(3000, function() {
     console.log('The BlogApp Server has started');
